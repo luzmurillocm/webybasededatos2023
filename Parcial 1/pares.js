13c21f13749af8c9b3f80da2c63b9ecd5d305226
luzmurillocm <124410747+luzmurillocm@users.noreply.github.com>
2023-09-11 19:18:35 -0700

    Add files via upload

--- Parcial 1/pares.js
+++ Parcial 1/pares.js
@@ -1,33 +1,60 @@
 $(document).ready(function (){
-    var par1="";
-    var par=false;
-    var par2="";
+    var animal1="";
+    var animal=false;
+    var animal2="";
     var total_pares=0;
     $('img').click(function(e){
         var estado= $(this).attr('data-estado')
         var nombre_imagen= $(this).attr('data-id')
+
+
         if(estado=="0"){
+            if(animal1!= ""&& animal2!=""){
+                console.log("las cartas son diferentes")
+                if(animal1.attr('data-id')!=animal1.attr('data-id')){
+                    console.log("tapa cartas diferentes")
+                    animal1.attr('src', 'imagenes/tapadas.jpg')
+                    animal2.attr('src', 'imagenes/tapadas.jpg')
+                }else{
+                    console.log("las cartas son pares")
+                    animal1.attr('data-estado', '1')
+                    animal2.attr('data-estado', '1')
+                    par=false
+                }
+                animal1=""
+                animal2=""
+            }
             console.log("estado de la carta: " + estado)
-            if(par1==""){
-                par1=$(this);
-                par1.attr('src', 'imagenes/' + nombre_imagen)
+            if(animal1==""){
+                animal1=$(this);
+                animal1.attr('src', 'imagenes/' + nombre_imagen)
                 console.log("se asigno par #1");
+                console.log(nombre_imagen)
             }else{
-                par1=$(this);
-                par1.attr('src', 'imagenes/' + nombre_imagen)
-                par1.attr('src', 'imagenes/' + nombre_imagen)
+                animal1=$(this);
+                animal1.attr('src', 'imagenes/' + nombre_imagen)
                 console.log("se asigno par #2");
             }
         }
-
-        if(par1!="" && par2 != ""){
-            if(par1.attr('data-id')==par2.attr('data-id')){
+        if(animal1="" && animal2 != ""){
+            if(animal1.attr('data-id')==animal2.attr('data-id')){
                 par = true
                 total_pares++
                 $("#total_pares").html(total_pares)
             }
         }
-
         
+        if(total_pares == 8){
+            $('img').each(function (){
+                $(this).attr('src', 'imagenes/tapadas.jpg')
+                $(this).attr('data-estado', '0')
+            });
+            total_pares=0
+            par=false
+            animal=""
+            animal2=""
+            return;
+        }
+
     });
 });
