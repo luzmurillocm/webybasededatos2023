31b627036ee1f71038f623620033869817346f90
luzmurillocm <124410747+luzmurillocm@users.noreply.github.com>
2023-09-12 19:13:19 -0700

    Add files via upload

--- Parcial 1/pares.js
+++ Parcial 1/pares.js
@@ -3,16 +3,16 @@ $(document).ready(function (){
     var par=false;
     var par2="";
     var total_pares=0;
+    var total_intentos=0;
     $('img').click(function(e){
         var estado= $(this).attr('data-estado')
         var nombre_imagen= $(this).attr('data-id')
 
 
-
         if(estado=="0"){
-            if(par1!= ""&& par2!=""){
+            if(par1!= "" && par2 != ""){
                 console.log("las cartas son diferente")
-                if(par1.attr('data-id')!=par1.attr('data-id')){
+                if(par1.attr('data-id') != par2.attr('data-id')){
                     console.log("tapa cartas diferentes")
                     par1.attr('src', 'imagenes/tap.jpg')
                     par2.attr('src', 'imagenes/tap.jpg')
@@ -26,35 +26,56 @@ $(document).ready(function (){
                 par2=""
           }
             console.log("estado de la carta: " + estado)
-            if(par1==""){
-                par1=$(this);
+            if(par1 == ""){
+                par1= $(this);
                 par1.attr('src', 'imagenes/' + nombre_imagen)
                 console.log("se asigno par #1");
-            }else{
-                par1=$(this);
-                par1.attr('src', 'imagenes/' + nombre_imagen)
-                par1.attr('src', 'imagenes/' + nombre_imagen)
+            }else if(par2 == ""){
+                par2=$(this);
+                par2.attr('src', 'imagenes/' + nombre_imagen)
                 console.log("se asigno par #2");
             }
         }
 
-        if(par1!="" && par2 != ""){
-            if(par1.attr('data-id')==par2.attr('data-id')){
+        if(par1 != "" && par2 != ""){
+            if(par1.attr('data-id') == par2.attr('data-id')){
                 par = true
                 total_pares++
                 $("#total_pares").html(total_pares)
+            } else {
+                total_intentos++
             }
         }
-        if(total_pares == 8){
+
+        if(total_intentos >= 5){
+            alert("excediste el numero de intentos permitidos")
+            alert("has perdido el juego")
             $('img').each(function (){
-                $(this).attr('src', 'imagenes/tapadas.jpg')
+                $(this).attr('src', 'imagenes/tap.jpg')
                 $(this).attr('data-estado', '0')
             });
             total_pares=0
+            total_intentos=0
+            $("#total_pares").html(total_pares)
             par=false
             parl=""
             par2=""
             return;
+     }                       
+        if(total_pares == 8){
+            alert("felicidades, has ganado el juego")
+            $('img').each(function (){
+                $(this).attr('src', 'imagenes/tap.jpg')
+                $(this).attr('data-estado', '0')
+            });
+            total_pares=0
+            total_intentos=0
+            $("#total_pares").html(total_pares)
+            par=false
+            parl=""
+            par2=""
+            return;
+
         }
         
     });
