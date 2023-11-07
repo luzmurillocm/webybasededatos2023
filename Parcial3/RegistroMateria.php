d6282a63e49751a4881b05d0d52752b547c27dbc
luzmurillocm <124410747+luzmurillocm@users.noreply.github.com>
2023-11-06 18:22:31 -0800

    Add files via upload

---
+++ Parcial3/RegistroMateria.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar Alumno</title>
+    <link rel="stylesheet" href="css/bootstrap.css"> 
+    <script src="code.jquery.com_jquery-3.7.1.js"></script>
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 card m-4 p-4">
+            <form action="">
+                <div class="form-group">
+                <label for="nombre">Nombre:</label>
+                        <input name="nombre" type="text" class="form-control" placeholder="Teclea la materia" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="semestre">Semestre:</label>
+                        <input name="semestre" type="number" class="form-control" placeholder="Teclea el semestre" required>
+                    </div>    
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
