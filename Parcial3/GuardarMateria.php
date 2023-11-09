ddb8b4f7107cf1952a1b957422eeb4bfa7278218
luzmurillocm <124410747+luzmurillocm@users.noreply.github.com>
2023-11-08 18:12:57 -0800

    Add files via upload

--- Parcial3/GuardarMateria.php
+++ Parcial3/GuardarMateria.php
@@ -1,23 +1,19 @@
 <?php
-
     include 'conexion.php';
 
-    $nombre = $_POST["nombre"];
-    $semestre = $_POST["semestre"];
-    $especialidad = $_POST["especialidad"];
+    $nombre=$_POST["nombre"];
+    $semestre=$_POST["semestre"];
+    $especialidad=$_POST["especialidad"];
 
-
-    $sql = "INSERT INTO materias(nombre, semestre, especialidad) VALUES ('" .$nombre."',".$semestre.",'".$especialidad."')";
-
-    if($conexion->query($sql) == TRUE){
-        header("Location: ConsultarMaterias.php");
+    $sql= "INSERT INTO materias(nombre, semestre, especialidad) VALUES ('".$nombre."',".$semestre.",'".$especialidad."')";
+    if($conexion->query($sql) === TRUE){
+        header("Location: consultarMaterias.php");
         $conexion->close();
         exit;
-    } else {
-        echo "<h2>Ocurrió un error</h2> <p>Error: " .$sql . "<br>" . $conexion->error . "</p>";
-        echo "<h3><a href='ConsultarMaterias.php'>Regresar a materias</a></h3>";
+    }else{
+        echo "<h2>Ocurrió un error</h2> <p>Error: ".$sql . "<br>" . $conexion->error . "</p>";
+        echo "<h3><a href='RegistrarMaterias.php'>Regresar a materias</a></h3>";
     }
 
 
-?>    
-
+?>
---
+++ Parcial3/eliminarMateria.php
@@ -0,0 +1,23 @@
+<?php
+
+    include 'conexion.php';
+
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM materias where id = ".$id;
+
+    if($conexion->query($sql) === TRUE){
+        header("Location: ConsultarMaterias.php");
+        $conexion->close();
+        exit;
+
+
+    }else{
+        echo"<h2>ocurrio un error</h2> <p>Error; " .$sql . "<br>" . $conexion->error . "</p>";
+        echo "<h3><a href='ConsultarMaterias.php'>Regresar a alumnos</a></h3>";
+        $conexion->close();
+    }
+
+
+
+?>
